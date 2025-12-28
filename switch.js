3a197ce50209d3b5c7782f0bfa9f06c58cbb01d7
khemraj <khemrajsaud56@gmail.com>
2025-12-28 14:07:01 +0545

    switchcase

---
+++ switch.js
@@ -0,0 +1,64 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const months = 3
+// switch (months) {
+//     case 1:
+//         console.log("january");
+        
+//         break;
+//     case 2:
+//         console.log("feb");
+        
+//         break;
+//     case 3:
+//         console.log("march");
+        
+//         break;
+//     case 5:
+//         console.log("appril");
+        
+//         break;
+//     case 6:
+//         console.log("may");
+        
+//         break;
+
+//     default:
+//         console.log("default case match");
+        
+//         break;
+// }
+
+
+const month = "march";
+switch (month) {
+    case "janaury":
+        console.log("janaury");
+        
+        break;
+    case "feb":
+        console.log("feb");
+        
+        break;
+    // case "march":
+    //     console.log("march");
+        
+        break;
+    case "april":
+        console.log("appril");
+        
+        break;
+
+    default:
+        console.log("default value");
+        
+        break;
+}
